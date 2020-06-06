48f5bb7123b0b02511268befb2836cb86dcf3ed6
devkjh92 <rewq9@naver.com>
2020-06-06 15:34:12 +0900

    상용한자추가

--- vue/src/words/CommChnChar/Mid/index.js
+++ vue/src/words/CommChnChar/Mid/index.js
@@ -2,6 +2,8 @@ import mid1a from './mid_1a'
 import mid1b from './mid_1b'
 import mid2a from './mid_2a'
 import mid2b from './mid_2b'
+import mid3a from './mid_3a'
+import mid3b from './mid_3b'
 
 export default {
   label: '상용한자(중학교)',
@@ -9,6 +11,8 @@ export default {
     mid1a,
     mid1b,
     mid2a,
-    mid2b
+    mid2b,
+    mid3a,
+    mid3b
   ]
 }
