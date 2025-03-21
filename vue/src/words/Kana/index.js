21c6a4d25b8667a7101c1fac6a1cb49d17fb9221
devkjh9 <devkjh9@gmail.com>
2025-03-21 20:25:09 +0900

    카타카나 추가

---
+++ vue/src/words/Kana/index.js
@@ -0,0 +1,10 @@
+import katakanaPron from './katakana_pron'
+import katakanaAlphabet from './katakana_alphabet'
+
+export default {
+  label: '가나',
+  words: [
+    katakanaPron,
+    katakanaAlphabet
+  ]
+}
--- vue/src/words/words.js
+++ vue/src/words/words.js
@@ -2,11 +2,13 @@ import CommChnCharLow from './CommChnChar/Low'
 import CommChnCharMid from './CommChnChar/Mid'
 import Grammar from './Grammar'
 import Jplt from './Jplt'
+import Kana from './Kana'
 import NHK from './NHK'
 import WordMaster from './WordMaster'
 
 export default {
   categories: [
+    Kana,
     CommChnCharLow,
     CommChnCharMid,
     Grammar,
